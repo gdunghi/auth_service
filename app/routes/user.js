2ff1c0c51d8f6f2b4f96e444e6f23f6e86683bf2
jirayu <jirayu.photisan@gmail.com>
2017-04-06 19:41:26 +0700

    refactor code

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-var User = require('../models/user'); // get our mongoose model
+const User = require('../models/user');
 
-
-// route to return all users (GET http://localhost:8080/api/users)
 router.get('/find', function (req, res) {
     User.find({}, function (err, users) {
         res.json(users);
--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-// =======================
-// get the packages we need ============
-// =======================
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const cors = require("cors");
-
 const tokenVerifier = require("./app/middleware/token.verifier");
 
-var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-var User = require('./app/models/user'); // get our mongoose model
-var apiRoutes = express.Router();
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const config = require('./config'); // get our config file
+const User = require('./app/models/user'); // get our mongoose model
+const apiRoutes = express.Router();
 
-const userRount = require("./app/routers/user");
+const userRoute = require("./app/routes/user");
 
-// =======================
-// configuration =========
-// =======================
-var port = process.env.PORT || 8081; // used to create, sign, and verify tokens
-mongoose.connect(config.database); // connect to database
-app.set('superSecret', config.secret); // secret variable
+
+let port = process.env.PORT || 8081;
+mongoose.connect(config.database);
+app.set('superSecret', config.secret);
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -44,12 +38,9 @@ app.use(cors({
 }));
 
 
-app.use("/api/users", tokenVerifier.verify, userRount);
+// TODO: route middleware to verify a token
+app.use("/api/users", tokenVerifier.verify, userRoute);
 
-// =======================
-// routes ================
-// =======================
-// basic route
 app.get('/', function (req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
@@ -57,7 +48,7 @@ app.get('/', function (req, res) {
 app.get('/setup', function (req, res) {
 
     // create a sample user
-    var nick = new User({
+    let nick = new User({
         name: 'Nick Cerminara',
         username: 'admin',
         password: 'password',
@@ -76,10 +67,6 @@ app.get('/setup', function (req, res) {
 });
 
 
-// API ROUTES -------------------
-// we'll get to these in a second
-// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
-// route to authenticate a user (POST http://localhost:8080/api/authenticate)
 apiRoutes.post('/authenticate', function (req, res) {
     // find the user
     User.findOne({
@@ -97,9 +84,7 @@ apiRoutes.post('/authenticate', function (req, res) {
                 res.json({ success: false, message: 'Authentication failed. Wrong password.' });
             } else {
 
-                // if user is found and password is right
-                // create a token
-                var token = jwt.sign({
+                let token = jwt.sign({
                     exp: Math.floor(Date.now() / 1000) + (60 + 60),
                     data: {
                         name: user.name,
@@ -109,7 +94,6 @@ apiRoutes.post('/authenticate', function (req, res) {
                         //expiresInMinutes: 1440 // expires in 24 hours
                     });
 
-                // return the information including token as JSON
                 res.json({
                     success: true,
                     message: 'Enjoy your token!',
@@ -122,32 +106,18 @@ apiRoutes.post('/authenticate', function (req, res) {
     });
 });
 
-
-// TODO: route middleware to verify a token
-
 // route to show a random message (GET http://localhost:8080/api/)
 apiRoutes.get('/', function (req, res) {
     res.json({ message: 'Welcome to the coolest API on earth!' });
 });
 
-// route to return all users (GET http://localhost:8080/api/users)
-apiRoutes.get('/users', function (req, res) {
-    User.find({}, function (err, users) {
-        res.json(users);
-    });
-});
-
 apiRoutes.get('/verify/:token', function (req, res) {
-    var token = req.params.token;
-    var decoded = jwt.verify(token, app.get('superSecret'));
+    let token = req.params.token;
+    let decoded = jwt.verify(token, app.get('superSecret'));
     res.json(decoded);
 });
 
-// apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
 
-// =======================
-// start the server ======
-// =======================
 app.listen(port);
 console.log('Magic happens at http://localhost:' + port);
